715feaa033c483d91a4891b9e7a45a51f04be5b6
Timothy H <timh1203@gmail.com>
2021-08-02 13:19:42 -0500

    Add styled-components, globalStyles, file cleanup

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.6rem;
+  }
+  h1 { font-size: 2.8rem; }
+  h2 { font-size: 2.4rem; }
+  h3 { font-size: 2rem; }
+`
