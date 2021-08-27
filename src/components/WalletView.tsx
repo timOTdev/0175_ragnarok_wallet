049983fee2cb3590e43d7379703885af773e4072
Tim H <timh1203@gmail.com>
2021-08-26 22:12:56 -0500

    Fix show my public key

--- src/components/WalletView.tsx
+++ src/components/WalletView.tsx
@@ -3,14 +3,14 @@ import AppContext from './AppContext';
 
 export default function WalletStats() {
   // @ts-ignore
-  const { publicKey, balance } = React.useContext(AppContext);
+  const { myPublicKey, balance } = React.useContext(AppContext);
   // @ts-ignore
 
   return (
     <>
       <h1>Wallet</h1>
       <h2>My public key:</h2>
-      <p>{publicKey}</p>
+      <p>{myPublicKey}</p>
       <h2>My private key:</h2>
       <h2>My current balance:</h2>
       <p>{balance} lamports</p>
