5495f92811a51386a01e3eca9583078c992430cc
Tim H <timh1203@gmail.com>
2021-09-06 15:43:36 -0500

    Add Footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,23 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.footer`
+  /* margin: 0 auto; */
+  /* margin-top: 2rem; */
+  /* display: flex; */
+  /* justify-content: space-around; */
+  /* align-items: center; */
+  /* text-align: center; */
   margin: 0 auto;
-  margin-top: 2rem;
   display: flex;
-  justify-content: space-around;
+  /* flex-direction: column; */
+  justify-content: center;
   align-items: center;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
 `;
 
 export default function Header(props: any) {
-  return <Wrapper>Footer</Wrapper>;
+  return (
+    <Wrapper>
+      <p>Made with 🍵, 🌮, 🌞 | Ragnarok Wallet © {new Date().getFullYear()}</p>
+    </Wrapper>
+  );
 }
