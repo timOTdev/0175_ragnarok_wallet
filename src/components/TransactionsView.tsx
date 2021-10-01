51518859bbd541a6e2ebffca0f0688be4d3d7969
Tim H <timh1203@gmail.com>
2021-09-30 21:59:09 -0500

    Fix transactionView useEffect issue

--- src/components/TransactionsView.tsx
+++ src/components/TransactionsView.tsx
@@ -1,43 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getRpcURL } from '../lib/utils';
 import { Connection, PublicKey } from '@solana/web3.js';
-import AppContext from './AppContext';
 import styled from 'styled-components';
-
-interface DataProp {
-  data: Array<TransactionProp>;
-  shouldReload: boolean;
-}
-
-// blockTime - estimated production time, as Unix timestamp (seconds since the Unix epoch) of when transaction was processed. null if not available.
-// slot -the period of time for which each leader ingests transactions and produces a block. Slots are ordered sequentially and non-overlapping, comprising roughly equal real-world time.
-// signature - transaction signature as base-58 encoded string
-interface TransactionProp {
-  blockTime: number;
-  confirmationStatus: string;
-  slot: number;
-  signature: string;
-}
-
-const getTransactions = (myPublicKey: PublicKey): any => {
-  // Grab the url from utils.
-  const url = getRpcURL();
-
-  // Check if url is not undefined.
-  const connection =
-    url !== undefined ? new Connection(url) : new Connection('');
-
-  const result = connection
-    .getSignaturesForAddress(myPublicKey, {
-      limit: 5,
-    })
-    .then((res) => {
-      console.log('res', res);
-      return res;
-    });
-
-  return result;
-};
+import AppContext from './AppContext';
 
 const Section = styled.section`
   border: 1px solid #333333;
@@ -71,6 +36,40 @@ const BodyCells = styled.td`
   padding: 2rem 4rem;
 `;
 
+interface DataProp {
+  data: Array<TransactionProp>;
+  shouldReload: boolean;
+}
+
+// blockTime - estimated production time, as Unix timestamp (seconds since the Unix epoch) of when transaction was processed. null if not available.
+// slot -the period of time for which each leader ingests transactions and produces a block. Slots are ordered sequentially and non-overlapping, comprising roughly equal real-world time.
+// signature - transaction signature as base-58 encoded string
+interface TransactionProp {
+  blockTime: number;
+  confirmationStatus: string;
+  slot: number;
+  signature: string;
+}
+
+const getTransactions = (myPublicKey: PublicKey): any => {
+  // Grab the url from utils.
+  const url = getRpcURL();
+
+  // Check if url is not undefined.
+  const connection =
+    url !== undefined ? new Connection(url) : new Connection('');
+
+  const result = connection
+    .getSignaturesForAddress(myPublicKey, {
+      limit: 5,
+    })
+    .then((res) => {
+      return res;
+    });
+
+  return result;
+};
+
 export default function TransactionsView() {
   let [state, setState] = useState<DataProp>({
     data: [],
@@ -78,27 +77,26 @@ export default function TransactionsView() {
   });
 
   // @ts-ignore
-  const { myPublicKey }: PublicKey = React.useContext(AppContext);
+  // For Checkpoint A
+  const { myPublicKey } = React.useContext(AppContext);
   // @ts-ignore
 
-  // //tbc Why doesn't plugging into new PublicKey doesn't work after refreshing page?
-  // console.log('publicKey', myPublicKey); // ARFZVbY4QXAgVmWPh79UcVJJhXVihtGyqcgx7GaqcaZL
-  // console.log('publicKey', typeof myPublicKey); // string
-
-  // Convert base-58 encoded string to PublicKey class.
-  const publicKey: PublicKey = new PublicKey(
-    'ARFZVbY4QXAgVmWPh79UcVJJhXVihtGyqcgx7GaqcaZL'
-  );
-
   useEffect(() => {
+    // Upon initial component load, myPublicKey not available yet.
+    // Just exit until it comes back from Context API.
+    // The dependency list holds this value and will re-render component when it comes in.
+    if (!myPublicKey) return;
+
+    // Create the PublicKey so we can use in getting the transactions.
+    const publicKey: PublicKey = new PublicKey(myPublicKey);
+
     getTransactions(publicKey).then((result: any) => {
-      console.log(result);
       setState({
         data: result,
         shouldReload: true,
       });
     });
-  }, [publicKey]);
+  }, [myPublicKey]);
 
   useEffect(() => {
     renderTransactions(state.data);
